f2e3bfb26b73c821186dfa6505902742f153f4f3
Héctor <hcastc00@estudiantes.unileon.es>
2021-05-30 14:40:30 +0200

    Implementado subida de archivos al servidor

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,17 +1,27 @@
-function registrar(){
-    console.log('Estoy registrando')
+$("#imagen").submit(function (e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr("action"),
+    type: $(this).attr("method"),
+    dataType: "file",
+    data: new FormData(this),
+    processData: false,
+    contentType: false,
+    complete: function(data) {
+       console.log(data)
+    },
+  });
+});
+
+function registrar() {
+  console.log("Estoy registrando");
 }
 
-function imagenIntroducida(){
-    console.log('Ha cambiado la imagen del formulario')
+function imagenIntroducida() {
+  console.log("Ha cambiado la imagen del formulario");
 }
 
-function enviaFormulario(){
-    console.log('Estoy enviando el formulario');
-    let fichero = document.getElementById('imagen').value;
-    console.log(fichero);
-
-    $.post("http://localhost/images", {file: fichero} , function(result){
-        console.log('La respuesta del server es: ', result);
-    });
+function enviaFormulario() {
+  console.log("Enviando");
+  $("#imagen").submit();
 }
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 const express = require("express");
-const application = express();
-
-const fs = require("fs");
-//const fs = fs();
-
+const app = express();
+const multer = require("multer");
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-application.use(express.urlencoded());
-
+app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
-application.use(express.json());
+app.use(express.json());
 
 // Configurar cabeceras y cors
-application.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST');
-    next();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST");
+  next();
 });
 
-application.use(express.static("public"));
+app.use(express.static("public"));
 
-application.listen(80, () => {
+app.listen(80, () => {
   console.log("El Kiosko ha abierto! 😈 Escuchando en http://localhost:80");
 });
 
-
-// Post para recibir imagenes introducidas por el admin
-application.post('/images', function(request, response){
-    //TODO no coge bien lo que manda el cliente
-    const filePath = request.body.file;
-    //TODO esto da uhndefined ni idea de por que
-    console.log(request.body.file);
-    //Express middleware gives a temporary name to the file, so we rename it.
-    fs.rename(filePath, "./" + filePath, function(err){
-        if(err){
-            response.send('Error' , err);
-        }
-        console.log("file uploaded. Extradata: tag = #{req.body.tag}");
-        response.send('Success');
-    });
+//Storage con multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "cromos");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-    
+var upload = multer({ storage: storage });
+
+// Post para recibir imagenes introducidas por el admin
+app.post("/uploadfile", upload.single("myFile"), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  res.send(200);
+});
